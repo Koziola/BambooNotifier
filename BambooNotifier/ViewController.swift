79a4935086aeeaf61fed2cc4bb1871ef82c3268a
Adam Koziol <adam.j.koziol@gmail.com>
2018-01-18 20:34:18 -0800

    add BambooNotifier project
    1. Skeleton status bar app that doesn’t have a main window or a dock icon

---
+++ BambooNotifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BambooNotifier
+//
+//  Created by Adam Koziol on 1/18/18.
+//  Copyright © 2018 Adam Koziol. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
